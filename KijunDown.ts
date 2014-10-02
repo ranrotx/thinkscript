cf895765c3213cef2397ab5425712338c2964f80
Ronnie Eichler <ronnie.eichler@gmail.com>
2014-10-01 21:23:59 -0500

    Added KijunDown.ts and KumoDown.ts

---
+++ KijunDown.ts
@@ -0,0 +1,23 @@
+# KijunDown.ts
+# Plots a red down arrow whenever there is a Kijun sell signal
+
+# Setup: Current day's candle closes below the Kijun.
+# Close from 2 days ago must be above the Kijun
+# Trigger: Chikou from 26 days ago must be below the Kumo of 26 days ago
+
+input tenkanPeriod = 9;
+input kijunPeriod = 26;
+
+def kijun = Ichimoku("tenkan period"=tenkanPeriod,"kijun period"=kijunPeriod).Kijun;
+def chikou = Ichimoku("tenkan period"=tenkanPeriod,"kijun period"=kijunPeriod).Chikou;
+def kumo = Ichimoku("tenkan period"=tenkanPeriod,"kijun period"=kijunPeriod)."Span A";
+
+def setupTrue = close[0] < kijun[0] and close[2] > kijun[2];
+def triggerTrue = chikou[26] < kumo[26];
+
+plot kijunDown = if setupTrue and triggerTrue then high * 0.990 else Double.NaN;
+kijunDown.SetPaintingStrategy(PaintingStrategy.BOOLEAN_ARROW_DOWN);
+kijunDown.SetLineWeight(5);
+kijunDown.AssignValueColor(Color.RED);
+kijunDown.HideBubble();
+kijunDown.HideTitle();
---
+++ KumoDown.ts
@@ -0,0 +1,22 @@
+# KumoDown.ts
+# Plots a red down arrow whenever there is a Kumo sell signal (breakout below the cloud)
+
+# Setup: Current day's candle closes below the Kumo cloud. Close from 4 days ago must
+# be in or above the cloud
+# Confirmation: Chikou from 26 days ago must be below the Kumo of 26 days ago
+
+input tenkanPeriod = 9;
+input kijunPeriod = 26;
+
+def chikou = Ichimoku("tenkan period"=tenkanPeriod,"kijun period"=kijunPeriod).Chikou;
+def kumo = Ichimoku("tenkan period"=tenkanPeriod,"kijun period"=kijunPeriod)."Span A";
+
+def setupTrue = close[0] < kumo[0] and close[4] > kumo[4];
+def confirmationTrue = chikou[26] < kumo[26];
+
+plot kumoDown = if setupTrue and confirmationTrue then low * 0.990 else Double.NaN;
+kumoDown.SetPaintingStrategy(PaintingStrategy.BOOLEAN_ARROW_DOWN);
+kumoDown.SetLineWeight(5);
+kumoDown.AssignValueColor(Color.RED);
+kumoDown.HideBubble();
+kumoDown.HideTitle();
