c9649f76b12753e3feef891c7af5d38bd44a15d9
Ronnie Eichler <ronnie.eichler@gmail.com>
2014-11-28 17:34:57 -0600

    Make cahnges to KumoDown.ts

--- KumoDown.ts
+++ KumoDown.ts
@@ -12,7 +12,7 @@ def chikou = Ichimoku("tenkan period"=tenkanPeriod,"kijun period"=kijunPeriod).C
 def kumoA = Ichimoku("tenkan period"=tenkanPeriod,"kijun period"=kijunPeriod)."Span A";
 def kumoB = Ichimoku("tenkan period"=tenkanPeriod,"kijun period"=kijunPeriod)."Span B";
 
-def setupTrue = Crosses(close[0], kumoB[0], CrossingDirection.BELOW) and close[4] > kumoB[4];
+def setupTrue = Crosses(close[0], kumoA[0], CrossingDirection.BELOW) and close[4] > kumoA[4] and kumoA[0] < kumoB[0];
 def confirmationTrue = chikou[26] < kumoA[26] and chikou[26] < kumoB[26];
 
 plot kumoDown = if setupTrue and confirmationTrue then low * 0.990 else Double.NaN;
